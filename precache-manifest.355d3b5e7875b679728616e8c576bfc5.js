0df9bb8c1412af8eee3a66a380b3bb2010a6eb54
Jeff Dam <jeff.dam@gmail.com>
2020-01-11 11:24:49 -0800

    Updates

---
+++ precache-manifest.355d3b5e7875b679728616e8c576bfc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f642029aa19a35c4b808170a0fa5d46",
+    "url": "/To-Do-List/index.html"
+  },
+  {
+    "revision": "cf911c6156e7bc5b4776",
+    "url": "/To-Do-List/static/css/main.ad3469f7.chunk.css"
+  },
+  {
+    "revision": "d2f8f073a23cf2cfbdb3",
+    "url": "/To-Do-List/static/js/2.c510a92d.chunk.js"
+  },
+  {
+    "revision": "e92e356b467ce5268290e03394dbf217",
+    "url": "/To-Do-List/static/js/2.c510a92d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cf911c6156e7bc5b4776",
+    "url": "/To-Do-List/static/js/main.c5472f0c.chunk.js"
+  },
+  {
+    "revision": "6f012880869113bb8e39",
+    "url": "/To-Do-List/static/js/runtime-main.8b1c40e4.js"
+  }
+]);
